30213dacdcb078bc2894b49b5dfe9020b427ff94
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-12-09 16:50:26 +0100

    feat(Linkedin API): 🇱🇳 Starting base to use Linkedin API
    Add LinkedinAPIConnection to have an accessToken
    Update
    getCandidateInfo
    to get user infos

--- backend/util/getCandidateInfo.py
+++ backend/util/getCandidateInfo.py
@@ -1,3 +1,7 @@
+import LinkedinAPIConnection
+import os
+import requests
+import re
 
 
 def getCandidateInfo(LinkedInURL):
@@ -6,4 +10,34 @@ def getCandidateInfo(LinkedInURL):
     :param LinkedInURL:
     :return: a diction
     """
-    raise NotImplementedError
+    clientID = os.getenv("LINKEDIN_CLIENT_ID")
+    clientSecret = os.getenv("LINKEDIN_CLIENT_SECRET")
+
+    token = LinkedinAPIConnection.getLinkedinToken(clientID, clientSecret)
+
+    if match := re.search(r"https://www.linkedin.com/in/(.*)/", LinkedInURL, re.IGNORECASE):
+        profileID = match.group(1)
+    elif match := re.search(r"www.linkedin.com/in/(.*)/", LinkedInURL, re.IGNORECASE):
+        profileID = match.group(1)
+    else:
+        profileID = LinkedInURL
+
+    url = f"https://api.linkedin.com/v2/people/(id:{profileID})?projection=(id,firstName,lastName)"
+    print(url)
+
+    payload = {}
+    files = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+    return response.text
+
+
+if __name__ == '__main__':
+    print("testing with : https://www.linkedin.com/in/titouan-joseph-revol/")
+    getCandidateInfo("https://www.linkedin.com/in/titouan-joseph-revol/")
+    print("testing with : www.linkedin.com/in/titouan-joseph-revol/")
+    getCandidateInfo("www.linkedin.com/in/titouan-joseph-revol/")
+    print("testing with : titouan-joseph-revol")
+    getCandidateInfo("titouan-joseph-revol")
